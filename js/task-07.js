4f22ded94c9778bfbe1d11e9c81375be3467e288
FilipMantaj <145697524+FilipMantaj@users.noreply.github.com>
2024-01-25 21:26:15 +0100

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,9 @@
 const input = document.querySelector("input#font-size-control");
 const span = document.querySelector("span#text");
-
-input.addEventListener("input", changeFontSize);
-
 function changeFontSize() {
   span.style.fontSize = input.value + "px";
 }
+input.addEventListener("input", changeFontSize);
+
+
+
